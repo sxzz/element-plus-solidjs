61443d8fe33faee21d4e2b09388801a625c93978
三咲智子 <sxzz@sxzz.moe>
2022-09-05 04:07:34 +0800

    chore: add inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import glob from 'unplugin-glob/vite'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [solidPlugin(), glob({ dts: true })],
+  plugins: [solidPlugin(), glob({ dts: true }), Inspect()],
 })
